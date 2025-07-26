5898742fd4933d17a014fa1387c59e5381031d2f
vinod-polinati <vinodd.polinati@gmail.com>
2025-07-26 12:44:47 +0530

    updated-llm-model

--- llm_service.py
+++ llm_service.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-GROQ_MODEL = os.getenv("GROQ_MODEL", "llama-3.3-70b-versatile")
+GROQ_MODEL = os.getenv("GROQ_MODEL", "llama-3.1-8b-instant")
 
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
 
